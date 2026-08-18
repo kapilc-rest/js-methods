8a1515ceafa8d661ef264e89472d5dafd5454e38
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-18 11:52:13 +0000

    Fix the filter parameters

--- filterRange.js
+++ filterRange.js
@@ -2,9 +2,8 @@ const array = [1, 2, 3, 4, 5, 6];
 
 function filterRange(arr,a,b) {
     //let n = arr.length();
-    let changedArray = arr.filter(a <= num && num >= b);
-    console.log(changedArray);
+    let changedArray = arr.filter((num) => a <= num && num <= b);
     return changedArray;
 }
 
-filterRange(array,2,5);
+console.log(filterRange(array,2,5));
