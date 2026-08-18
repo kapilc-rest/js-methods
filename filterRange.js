0f9b44565460aa025bb9a07f34676890bf7fd88c
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-18 11:54:38 +0000

    Remove changedArray noise from function

--- filterRange.js
+++ filterRange.js
@@ -1,7 +1,7 @@
 const array = [1, 2, 3, 4, 5, 6];
 
 function filterRange(arr,a,b) {
-    return changedArray = arr.filter((num) => a <= num && num <= b);
+    return arr.filter((num) => a <= num && num <= b);
 }
 
 console.log(filterRange(array,2,5));
