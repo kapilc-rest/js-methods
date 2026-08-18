fbbe9b40732a4e7fdd9fcbfd7df64e9673fba100
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-18 11:53:16 +0000

    Merge the lines into one in function

--- filterRange.js
+++ filterRange.js
@@ -1,9 +1,7 @@
 const array = [1, 2, 3, 4, 5, 6];
 
 function filterRange(arr,a,b) {
-    //let n = arr.length();
-    let changedArray = arr.filter((num) => a <= num && num <= b);
-    return changedArray;
+    return changedArray = arr.filter((num) => a <= num && num <= b);
 }
 
 console.log(filterRange(array,2,5));
