0d8d6f60af8c80b1c378a216abe0ebf1e1b20cd7
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-19 15:06:51 +0000

    Make the code neater

--- decreasingSort.js
+++ decreasingSort.js
@@ -1,11 +1,3 @@
 let array = [5, 2, 1, -10, 8];
 
-function compareNum (a,b) {
-    if (a > b) return -1;
-    if ( a == b) return 0;
-    if ( a < b ) return 1;
-}
-
-array.sort(compareNum);
-
-console.log(array);
+console.log(array.sort((a,b) => b -a));
