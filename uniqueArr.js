37edfe55762522425624dcc7ea43f439cbe4dd5f
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-19 18:16:56 +0000

    Make the function concise

--- uniqueArr.js
+++ uniqueArr.js
@@ -1,16 +1,7 @@
 let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
 
 function unique(arr) {
-    let newArr = strings.slice();
-    for(let i = 0; i<newArr.length; i++) {
-        for(let j = i+1; j < newArr.length; j++) {
-            if (newArr[i] == newArr[j]) {
-                newArr.splice(j,1)
-                j--;
-            }
-        }
-    }
-    return newArr;
+    return arr.filter((item, index) => arr.indexOf(item) == index);
 }
 
 console.log(unique(strings));
