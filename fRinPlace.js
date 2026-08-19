aacc9fb13f55a6ff213ff963ffc523eb9d0a6b84
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-19 12:33:36 +0000

    Add && in filter cause JS doesn't hold chained conditions

--- fRinPlace.js
+++ fRinPlace.js
@@ -1,5 +1,9 @@
+const array = [1, 2, 3, 4, 5, 6]
+
 function filterRangeInPlace(arr,a,b) {
-    const array = [1, 2, 3, 4, 5, 6]
-    arr = arr.filter((num, i) => a <= num <=b);
-    console.log(filterRangeInPlace(arr,2,5));
+    let p = arr;
+    p = arr.filter((num) => a <= num && num <=b);
+    console.log(arr);
 }
+
+filterRangeInPlace(array,2,5);
