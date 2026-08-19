a11ed583c2f71715bc6200e01923c77d7f285e0d
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-19 14:25:55 +0000

    FIx the function

--- fRinPlace.js
+++ fRinPlace.js
@@ -1,9 +1,18 @@
-const array = [1, 2, 3, 4, 5, 6]
+const array = [1, 3, 6, 2, 7, 4]
 
 function filterRangeInPlace(arr,a,b) {
-    let p = arr;
-    p = arr.filter((num) => a <= num && num <=b);
-    console.log(arr);
+    for (let i = 0; i < arr.length; i++) {
+        if(a <= arr[i] && arr[i] <=b) {
+            continue;
+        }
+        else {
+            arr.splice(i,1);
+            i--;
+        }
+
+    }
 }
 
-filterRangeInPlace(array,2,5);
+filterRangeInPlace(array,2,5)
+
+console.log(array);
