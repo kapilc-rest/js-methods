a5fd0d4f06371648937f03c47cd258e0c9911fa6
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-31 05:45:05 +0000

    Go throught some manipulation techniques

--- dom.js
+++ dom.js
@@ -10,5 +10,11 @@ const display2 = controls.previousElementSibling;
 console.log(display2); // <div class="display"></div>
 
 const div = document.createElement("div");
-div.style.color = "blue";
-div.setAttribute("style", "color: blue; background: white;");
+div.setAttribute("style", "color: blue");
+div.textContent = "Hello!";
+container.insertBefore(div, controls);
+div.setAttribute("id", "theDiv");
+console.log(div.getAttribute("id"));
+div.removeAttribute("id");
+div.removeAttribute("class"); //nothing happens cause it doesnt even have class to begin with
+div.innerHTML="<span>Hello world</span>";
