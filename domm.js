3bc729d3904bc49f6e4b746428841f591f4fb8b8
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-31 06:15:52 +0000

    Add h1 and para to newDiv

--- domm.js
+++ domm.js
@@ -1,3 +1,12 @@
+/*Exercise: Add the following elements to the container using ONLY JavaScript and the DOM methods shown above:
+
+a <p> with red text that says “Hey I’m red!”
+an <h3> with blue text that says “I’m a blue h3!”
+a <div> with a black border and pink background color with the following elements inside of it:
+another <h1> that says “I’m in a div”
+a <p> that says “ME TOO!”
+Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container. */
+
 const container = document.querySelector("#container");
 
 const para = document.createElement("p");
@@ -14,3 +23,12 @@ const newDiv = document.createElement("div");
 newDiv.style.border = "solid 1px black";
 newDiv.style.backgroundColor = "pink";
 container.appendChild(newDiv);
+
+const heading1 = document.createElement("h1");
+heading1.textContent = "I'm in a div";
+
+const paraInDiv = document.createElement("p");
+paraInDiv.textContent = "ME TOO!";
+
+newDiv.appendChild(heading1);
+newDiv.appendChild(paraInDiv);
