781fc6efd711932b34fc4877aa2c8db9c37227a8
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-31 06:02:22 +0000

    Add subheading to container

--- domm.js
+++ domm.js
@@ -1,7 +1,11 @@
+const container = document.querySelector("#container");
+
 const para = document.createElement("p");
 para.textContent = "Hey I'm red!";
 para.style.color = "red";
-
-const container = document.querySelector("#container");
-
 container.appendChild(para);
+
+const subHeading3 = document.createElement("h3");
+subHeading3.style.color = "blue";
+subHeading3.textContent = "I'm blue h3";
+container.appendChild(subHeading3);
